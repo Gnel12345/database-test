ba336994132f6ad9ff42712eb057f827fb6b3988
Glenn Nelson <gnel1234@yahoo.com>
2018-04-23 06:37:21 -0500

    -m added InnerJoin

--- src/databaseTest/DatabaseTestMain.java
+++ src/databaseTest/DatabaseTestMain.java
@@ -16,10 +16,13 @@ public class DatabaseTestMain {
 	public void dbtest() throws SQLException{
 		String host = "localhost";
 		String port = "3306";
-		
+	//sets the connection to the database	
 	Connection con=	DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/emp_datab","root","root" );
+	//creates statement
 	Statement s = con.createStatement();
+	//executes query
 	ResultSet rs = s.executeQuery("select * from Employee_Table");
+	//goes through whole table and prints it to console or TestNG
 	while(rs.next()){
 		
 		System.out.println(rs.getString("name"));
@@ -30,5 +33,29 @@ public class DatabaseTestMain {
 	}
 	
 	}
+	//Inner Join
+	@Test
+	public void dbtest2() throws SQLException{
+		String host = "localhost";
+		String port = "3306";
+	//sets the connection to the database	
+	Connection con=	DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/empdata","root","root" );
+	//creates statement
+	Statement s = con.createStatement();
+	//executes query
+	ResultSet rs = s.executeQuery("select Employee.firstname, Employee.lastname, EmployeeSalary.Salary From Employee Employee Inner Join EmployeeSalary ON Employee.id = EmployeeSalary.id");
+	//goes through whole table and prints it to console or TestNG
+	while(rs.next()){
+		
+		
+		//System.out.println(rs.getString("id"));
+		System.out.println(rs.getString("firstname"));
+		System.out.println(rs.getString("lastname"));
+		System.out.println(rs.getString("salary"));
+	}
+	
+	}
+	
+	
 	
 }
